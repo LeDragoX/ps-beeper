7dc3329f2ccb209a33ac2e9ccb1a301f32af1585
Plínio Larrubia <ledragox.contact@gmail.com>
2024-08-10 23:12:06 -0300

    Add support to more one-line notes on Start-Beep - This feature will help save time when loading new music notes

--- src/lib/Start-Beep.psm1
+++ src/lib/Start-Beep.psm1
@@ -18,8 +18,16 @@ function Start-Beep() {
         [Switch] $Wait
     )
 
+    $Note = $Note.Trim()
+
+    If ($Note -like '* *') {
+        Write-Host "[$($MyInvocation.MyCommand.Name)] '$Note' contains whitespace!!! Isolating and running..." -ForegroundColor Yellow
+        $Note -split '\s' | ForEach-Object { Start-Beep -G $Glossary -N $_ -D $Duration -Wait }
+        return
+    }
+
     If ($Frequency -and $Duration) {
-        Write-Host "[$($MyInvocation.MyCommand.Name)] Note: $Frequency Hz: $Duration`ms"
+        Write-Host "[$($MyInvocation.MyCommand.Name)] Note Frequency $Frequency`Hz: $Duration`ms"
         [System.Console]::Beep($Frequency, $Duration);
     }
     If ($Note -and $Duration) {
@@ -27,12 +35,12 @@ function Start-Beep() {
         $NoteOctave = $Note -replace '\D'
         $Frequency = $Glossary[$NoteName][$NoteOctave]
         If ($Delay -eq 0) {
-            Write-Host "[$($MyInvocation.MyCommand.Name)] Note: $NoteName$NoteOctave`: $Frequency`Hz in $Duration`ms"
+            Write-Host "[$($MyInvocation.MyCommand.Name)] Note <$NoteName$NoteOctave> $Frequency`Hz in $Duration`ms"
         }
         [System.Console]::Beep($Frequency, $Duration);
 
         If ($Delay -gt 0) {
-            Write-Host "[$($MyInvocation.MyCommand.Name)] Note: $NoteName$NoteOctave`: $Frequency`Hz in $Duration`ms + $Delay`ms"
+            Write-Host "[$($MyInvocation.MyCommand.Name)] Note <$NoteName$NoteOctave> $Frequency`Hz in $Duration`ms + $Delay`ms"
             Start-Sleep -Milliseconds $Delay
         } ElseIf ($Wait) {
             Read-Host -Prompt "[$($MyInvocation.MyCommand.Name)] > Press ENTER to continue"
