89f82b7d8856c9de92586edff54cd43be265c410
Plínio Larrubia <ledragox.contact@gmail.com>
2024-08-15 00:58:53 -0300

    Add Chainsaw Man op FROM SCRATCH - Add note by note, split into parts - Still need to be 1 higher octave on the chorus

---
+++ src/music/chainsaw-man-op.ps1
@@ -0,0 +1,67 @@
+Import-Module "$PSScriptRoot\..\lib\Convert-ToFrequency.psm1" -Function Convert-ToFrequency -PassThru -Force
+Import-Module "$PSScriptRoot\..\lib\Read-Glossary.psm1" -Function Read-Glossary -PassThru -Force
+Import-Module "$PSScriptRoot\..\lib\Start-Beep.psm1" -Function Start-Beep -PassThru -Force
+
+$Delay = 0
+$GlossaryHash = Read-Glossary
+$OutputFile = "$(Split-Path "$PSCommandPath" -Leaf)-frequency.ps1"
+$TestTime = 120
+
+# By Kenshi Yonezu / Adapted by Me
+$NotesIntro1 = @"
+A#4 A#4 A#4 C#5 A#4 C5 F4 A4 F4 G4 A4
+A#4 A#4 A#4 C#5 A#4 C5 F4 A5 F#5 A5 F#5 A5 Bb5
+A#4 A#4 A#4 C#5 A#4 C5 F4 C5 Eb5 C5 Eb5
+A#4 A#4 A#4 C#5 A#4 C5 F4 A4 F4 G4 A4
+
+A4 Bb4 A4 Bb4 A4 A4 A4 A4 A4
+A4 Bb4 A4 Bb4 A4 A4 A4 A4 A4
+A4 Bb4 A4 Bb4 A4 A4 A4 A4 A4
+A4 Bb4 A4 Bb4 A4 A4 A4 A4 A4
+
+F5 E5 F5 E5 A4 A#4 C5 C5 C#5 C#5 F#4 F4
+F5 E5 F5 E5 A4 A#4 C5 C5 C5 C#5 C#5 C#5 Eb5 C5 C5
+"@
+
+$NotesIntro2 = @"
+A#4 A4 A#4 D5 C#5 D5
+E5 F5 E5 C#5 D5 E5
+D5 Bb4 G4 F#4 G4 A4 G4
+G4 F#4 G4 A4 Bb4
+Bb4 A4 Bb4 C5 D5 Bb4 C5 G#4 G4
+
+G#4 Bb4 B4 Bb4 G#4 F#4 G#4
+G#4 Eb5 C#5 Bb4 B4 B4
+B4 B4 F#5 E5 D#5 C#5 C5 C5
+G#4 F#4 G#4 A4 B4 C#5 Eb5 C5 G#4 E5 Eb5
+"@
+
+$NotesChorus = @"
+G#5 C#5 Eb5 E5 F#5 B4 E5 Eb5 C#5 C5 A4 A#4 G#4 G4 C#5
+G#4 F#4 E4 F#4 E4 F#4 G#4 A4 B4 A4 G#4 F#4 C#5 C5 A4 G#4 E5 Eb5
+G#4 C#4 Eb4 E4 F#4 B3 E4
+Eb4 C#4 C4 A4 G#4 F#4 G#4 F#4 F4 Eb4 E4
+E4 Eb4 B3 C#4 Bb3 C#4 C#4
+E4 Eb4 B3 C#4 Bb3 C#4 C#4
+G#4 G4 G#4 G4 G#4 G4 G#4
+G#4 F#4 E4 F#4 E4 Eb4 B3
+"@
+
+$NotesEnd = @"
+A4 Bb4 A4 Bb4 A4 A4 A4 A4 A4
+A4 Bb4 A4 Bb4 A4 A4 A4 A4 A4
+A4 Bb4 A4 Bb4 A4 A4 A4 A4 A4
+A4 Bb4 A4 Bb4 A4 A4 Bb4 Bb4 B4
+"@
+
+function Convert-Music() {
+    Convert-ToFrequency -Glossary $GlossaryHash -N "$NotesIntro1 $NotesIntro2 $NotesChorus $NotesEnd" -D 120 -O "$OutputFile" -PCBeeperMode
+}
+
+function Play-Music() {
+    Start-Beep -Glossary $GlossaryHash -N "$NotesIntro1 $NotesIntro2 $NotesChorus $NotesEnd" -D $TestTime -Wait
+}
+
+Clear-Host
+Convert-Music
+Play-Music
